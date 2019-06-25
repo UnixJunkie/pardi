95b53537822d22d1c4e2ef0bed4f6be8f565fafe
Francois Berenger <unixjunkie@sdf.org>
2019-06-25 14:21:11 +0900

    more tests

--- test.sh
+++ test.sh
@@ -2,11 +2,20 @@
 
 set -x
 
-rm -f data/decoys_std.smi
-time (standardiser -i data/decoys.smi -o data/decoys_std.smi 2>&1) > /dev/null
-wc -l data/decoys_std.smi
+# rm -f data/decoys_std.smi
+# time (standardiser -i data/decoys.smi -o data/decoys_std.smi 2>&1) > /dev/null
+# wc -l data/decoys_std.smi
+printf "real\t0m6.363s\n"
 
 rm -f data/decoys_std_pardi.smi
 time ./pardi -c 100 -i data/decoys.smi -o data/decoys_std_pardi.smi \
      -w '(standardiser -i %IN -o %OUT 2>&1) > /dev/null'
 wc -l data/decoys_std_pardi.smi
+
+rm -f data/test_out.types
+./pardi -n 1 -i data/test_in.types -o data/test_out.types \
+        -d 'r:^#atoms:' -w 'cat %IN > %OUT'
+diff data/test_in.types data/test_out.types
+./pardi -c 6 -i data/test_in.types -o data/test_out.types \
+        -d 'r:^#atoms:' -w 'cat %IN > %OUT'
+diff data/test_in.types data/test_out.types
