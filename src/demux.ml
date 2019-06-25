bad3e5d2eb9ff7ff3ffac85d02f7794d1cf9479d
Francois Berenger <unixjunkie@sdf.org>
2019-06-25 12:39:42 +0900

    Demux: parsing of the -d|--demux option

--- src/demux.ml
+++ src/demux.ml
@@ -2,16 +2,17 @@
 (* how to cut the input file into independant items *)
 
 type t = Line (* default (e.g. for SMILES files) *)
-       | Sep of string (* (MOL2, SDF, PDB, etc.) *)
-       | Bytes of int (* fixed block size *)
-       | Reg of string (* a line-matching regexp separator *)
+       | Sep of string (* a line separator, at the end of each record
+                          (MOL2, SDF, PDB, etc.) *)
+       | Bytes of int (* a constant block size *)
+       | Reg of Str.regexp (* a line-matching regexp *)
 
 let of_string = function
   | "l" -> Line
   | demux_opt ->
-    let head, tail = BatString.split demux_opt ~by":" in
+    let head, tail = BatString.split demux_opt ~by:":" in
     match head with
     | "b" -> Bytes (int_of_string tail)
     | "s" -> Sep tail
-    | "r" -> Reg tail
-    | _ -> failwith ("Demux.of_string: unknown demux mode: " ^ tail)
+    | "r" -> Reg (Str.regexp tail)
+    | _ -> failwith ("Demux.of_string: unknown demux mode: " ^ demux_opt)
