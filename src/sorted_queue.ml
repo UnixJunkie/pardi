a94d1761a137d0609523e183cead8b25d14b2700
Francois Berenger <unixjunkie@sdf.org>
2019-06-27 11:35:42 +0900

    Sorted_queue: way simpler implementation

--- src/sorted_queue.ml
+++ src/sorted_queue.ml
@@ -1,41 +1,29 @@
 
-(* a queue so that jobs done coming in a random order are put back in input order
-   before being muxed; for Mux.Sort_cat_into mux mode *)
+(* a queue so that results coming in random order are put back in input order
+   before being muxed out (for the Mux.Sort_cat_into output mode) *)
+
+module Ht = Hashtbl
 
 type 'a t = { mutable next_rank: int;
-              mutable q: (int * 'a) list }
+              q: (int, 'a) Ht.t }
 
 let create () =
   let next_rank = 0 in
-  let q = [] in
+  let q = Ht.create 1021 in
   { next_rank; q }
 
-let insert_into (rank, x) l =
-  let rec loop acc = function
-    | [] -> List.rev_append acc [(rank, x)]
-    | (rank', y) :: rest ->
-      if rank' < rank then
-        loop ((rank', y) :: acc) rest
-      else if rank' = rank then
-        assert(false) (* no two elements are allowed to have the same prio *)
-      else (* rank' > rank *)
-        List.rev_append acc ((rank, x) :: (rank', y) :: rest)
-  in
-  loop [] l
-
 let insert (rank, elt) q =
-  q.q <- insert_into (rank, elt) q.q
+  assert(not (Ht.mem q.q rank)); (* no two elements can have same rank *)
+  Ht.add q.q rank elt
 
 let pop q =
-  match q.q with
-  | [] -> None
-  | (rank, elt) :: xs ->
-    if rank <> q.next_rank then None
-    else
-      let res = Some elt in
-      q.q <- xs;
+  match Ht.find_opt q.q q.next_rank with
+  | None -> None
+  | Some x ->
+    begin
       q.next_rank <- q.next_rank + 1;
-      res
+      Some x
+    end
 
 let pop_all q =
   let rec loop acc =
