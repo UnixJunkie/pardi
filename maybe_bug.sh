5c06b8f2d40d73596dc70523147ebe0dd5b01cca
Francois Berenger <unixjunkie@sdf.org>
2019-06-27 17:23:34 +0900

    comment about bug repro

--- maybe_bug.sh
+++ maybe_bug.sh
@@ -2,3 +2,5 @@
 
 ./pardi -c 10 -i data/decoys.mol2 -o data/decoys2.mol2 -d b:1000 \
         -w 'in=%IN; out=%OUT; /bin/cp $in $out; ls -l $in $out' -m s > log
+
+# if you get a 'No such file or directory', the bug just happened
