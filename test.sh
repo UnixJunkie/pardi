851660cdb374bc30f46111e1f03c0135b1f4dc2c
Francois Berenger <unixjunkie@sdf.org>
2019-07-04 17:55:08 +0900

    tests: exercize new options: -ie, -oe and -t

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ set -x
 printf "real\t0m6.363s\n"
 
 rm -f data/decoys_std_pardi.smi
-time ./pardi -c 100 -i data/decoys.smi -o data/decoys_std_pardi.smi \
+time ./pardi -v -c 100 -i data/decoys.smi -ie .smi -oe .smi -o data/decoys_std_pardi.smi \
      -w '(standardiser -i %IN -o %OUT 2>&1) > /dev/null'
 wc -l data/decoys_std_pardi.smi
 
@@ -28,5 +28,5 @@ grep -c '@<TRIPOS>MOLECULE' data/decoys.mol2 data/still_decoys.mol2
 # test sorted cat mux mode
 rm -f data/decoys2.mol2
 ./pardi -i data/decoys.mol2 -o data/decoys2.mol2 \
-        -d b:10000 -w 'cp %IN %OUT' -m s
+        -d b:10000 -w 'cp %IN %OUT' -m s -t 475 # there are 475 blocks of 10_000 B
 diff -q data/decoys.mol2 data/decoys2.mol2
