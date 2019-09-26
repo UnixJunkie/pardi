e5fd1e633d28fec136c6a7fc76fdf80bb18b2f31
Francois Berenger <unixjunkie@sdf.org>
2019-09-26 16:02:40 +0900

    rm src/messages.ml

--- src/messages.ml
+++
@@ -1,7 +0,0 @@
-
-type from_serv =
-  | Serv_to_CLI_DHKE_params_bcast of Cryptokit.DH.parameters
-  | Serv_to_CLI_DHKE_handshake of string
-
-type from_cli =
-  | CLI_to_Serv_DHKE_handshake of string
