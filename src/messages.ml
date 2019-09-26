b079dc46fd60b5706466906f21937c2e1da07564
Francois Berenger <unixjunkie@sdf.org>
2019-09-26 15:38:49 +0900

    Messages: WIP

--- src/messages.ml
+++ src/messages.ml
@@ -1,3 +1,7 @@
 
-type t =
-  | Serv_to_CLI_DHKE_params of Cryptokit.DH.parameters
+type from_serv =
+  | Serv_to_CLI_DHKE_params_bcast of Cryptokit.DH.parameters
+  | Serv_to_CLI_DHKE_handshake of string
+
+type from_cli =
+  | CLI_to_Serv_DHKE_handshake of string
